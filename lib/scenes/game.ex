153057ab8d0949e08150b241518c2daa805359d8
Jon Anderson <jon.david.anderson@gmail.com>
2018-11-29 17:52:55 -0800

    Started implementing the game.
    Currently, the snake moves across the screen and loops back around. The
    score is displayed at the top. No user controls implemented yet.

---
+++ lib/scenes/game.ex
@@ -0,0 +1,96 @@
+defmodule Snake.Scene.Game do
+  use Scenic.Scene
+  alias Scenic.Graph
+  alias Scenic.ViewPort
+  import Scenic.Primitives, only: [rrect: 3, text: 3]
+
+  @graph Graph.build font: :roboto, font_size: 36
+  @tile_size 32
+  @tile_radius 8
+  @snake_starting_size 5
+  @frame_ms 192
+
+  def init(_arg, opts) do
+    viewport = opts[:viewport]
+
+    {:ok, %ViewPort.Status{size: {vp_width, vp_height}}} = ViewPort.info(viewport)
+    vp_tile_width = trunc(vp_width / @tile_size)
+    vp_tile_height = trunc(vp_height / @tile_size)
+
+    snake_start_coords = {
+      trunc(vp_tile_width / 2),
+      trunc(vp_tile_height / 2)
+    }
+
+    {:ok, timer} = :timer.send_interval(@frame_ms, :frame)
+
+    state = %{
+      viewport: viewport,
+      tile_width: vp_tile_width,
+      tile_height: vp_tile_height,
+      graph: @graph,
+      frame_count: 1,
+      frame_timer: timer,
+      score: 0,
+      objects: %{
+        snake: %{
+          body: [snake_start_coords],
+          size: @snake_starting_size,
+          direction: {1, 0}
+        }
+      }
+    }
+
+    state.graph
+    |> draw_score(state.score)
+    |> draw_game_objects(state.objects)
+    |> push_graph
+
+    {:ok, state}
+  end
+
+  def handle_info(:frame, %{frame_count: frame_count} = state) do
+    state = move_snake(state)
+
+    state.graph
+    |> draw_game_objects(state.objects)
+    |> draw_score(state.score)
+    |> push_graph
+
+    {:noreply, %{state | frame_count: frame_count + 1}}
+  end
+
+  defp move_snake(%{objects: %{snake: snake}} = state) do
+    [head | _] = snake.body
+    new_head_pos = move(state, head, snake.direction)
+
+    new_body = Enum.take [new_head_pos | snake.body], snake.size
+    put_in(state, [:objects, :snake, :body], new_body)
+  end
+
+  defp move(%{tile_width: w, tile_height: h}, {pos_x, pos_y}, {vec_x, vec_y}) do
+    {rem(pos_x + vec_x + w, w), rem(pos_y + vec_y + h, h)}
+  end
+
+  defp draw_score(graph, score) do
+    graph
+    |> text("Score: #{score}", fill: :white, translate: {@tile_size, @tile_size})
+  end
+
+  defp draw_game_objects(graph, object_map) do
+    Enum.reduce(object_map, graph, fn {object_type, object_data}, graph ->
+      draw_object(graph, object_type, object_data)
+    end)
+  end
+
+  defp draw_object(graph, :snake, %{body: snake}) do
+    Enum.reduce(snake, graph, fn {x, y}, graph ->
+      draw_tile(graph, x, y, fill: :lime)
+    end)
+  end
+
+  defp draw_tile(graph, x, y, opts) do
+    tile_opts = Keyword.merge([fill: :white, translate: {x * @tile_size, y * @tile_size}], opts)
+    graph |> rrect({@tile_size, @tile_size, @tile_radius}, tile_opts)
+  end
+end
